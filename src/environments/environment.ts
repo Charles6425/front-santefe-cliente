3b260596acfc9280a440bcb38a64aafeb1a2464c
davidbs09 <davidbissacodasilva@gmail.com>
2025-08-24 18:07:00 -0300

    fix: ajuste de url para cloud front aws

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  baseUrl: 'http://localhost:8080', // Mantendo para compatibilidade
-  apiUrl: 'http://localhost:8080',
+  baseUrl: 'http://localhost:8080/api',
+  apiUrl: 'http://localhost:8080/api',
   cartKey: 'cliente_cart',  // Chave específica do cliente
   userType: 'CLIENTE'
 };
